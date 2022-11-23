8977b7b6eaa3f9b5144c3bdf92afd07be7a2c533
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2022-11-23 14:28:12 -0400

    feat: add routes

--- index.js
+++ index.js
@@ -2,10 +2,21 @@ const express = require('express')
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req,res)=>{
-    res.json('Página Inicial')
+const { product } = require('./models')
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get('/', async (req,res)=>{
+  const returnProducts = await product.findAll();
+  res.json(returnProducts)
 })
 
+app.post('/', async (req,res)=>{
+    const createProduct = product.create(req.body)
+    res.json(createProduct)
+})
 
 app.listen(PORT, ()=>{
     console.log(`O servidor está rodando na porta ${PORT}`)
