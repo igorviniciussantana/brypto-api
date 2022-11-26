4b186aa5ac08008aba54058f5fac8b17d1b27798
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2022-11-26 13:37:40 -0400

    fix: fix db connection error

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ require('dotenv/config');
 
 module.exports = {
   development: {
-    username: process.env.USERNAME,
+    username: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
     host: process.env.HOST,
