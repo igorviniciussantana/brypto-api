89e00091cb89c6e80ce87056435053e585f01f84
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2022-11-30 14:35:24 -0300

    fix: fix database config

--- config/config.js
+++ config/config.js
@@ -11,7 +11,7 @@ module.exports = {
   },
 
   test: {
-    username: process.env.USERNAME,
+    username: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
     host: process.env.HOST,
@@ -20,7 +20,7 @@ module.exports = {
   },
   
   production: {
-    username: process.env.USERNAME,
+    username: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
     host: process.env.HOST,
