a97893f11b82e70588ed886f2d3c29d5030e3db1
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2022-11-30 15:38:34 -0300

    fix: fix database

--- index.js
+++ index.js
@@ -12,33 +12,33 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*")
     app.use(cors());
     next();
-  });
+});
 
-app.get('/', async (req,res)=>{
- res.json('Página Inicial');
+app.get('/', async (req, res) => {
+    res.json('Página Inicial');
 })
 
-app.get('/products', async (req,res)=>{
-  const returnProducts = await product.findAll();
-  res.json(returnProducts)
+app.get('/products', async (req, res) => {
+    const returnProducts = await product.findAll();
+    res.json(returnProducts)
 })
 
-app.post('/products', async (req,res)=>{
+app.post('/products', async (req, res) => {
     const createProduct = await product.create(req.body)
     res.json(createProduct)
 })
 
-app.get('/products/:id', async (req,res)=>{
+app.get('/products/:id', async (req, res) => {
     const findProduct = await product.findByPk(req.params.id)
     res.json(findProduct)
 })
 
-app.delete('/products/:id', async (req,res)=>{
+app.delete('/products/:id', async (req, res) => {
     const deleteProduct = await product.findByPk(req.params.id)
     deleteProduct.destroy()
     res.json('Deleted')
 })
 
-app.listen(3000, ()=>{
+app.listen(8999, () => {
     console.log(`O servidor está rodando na porta 3000`)
 })
